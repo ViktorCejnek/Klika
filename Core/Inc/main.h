2db09a828db769c90da5cab542be7e96fe62c720
ViktorCejnek <viktor.cejnek@seznam.cz>
2024-05-05 12:44:17 +0200

    LED_Blink

---
+++ Core/Inc/main.h
@@ -0,0 +1,97 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wbxx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define MS1_Pin GPIO_PIN_0
+#define MS1_GPIO_Port GPIOA
+#define MS2_Pin GPIO_PIN_1
+#define MS2_GPIO_Port GPIOA
+#define SPREAD_Pin GPIO_PIN_4
+#define SPREAD_GPIO_Port GPIOA
+#define ENN_Pin GPIO_PIN_5
+#define ENN_GPIO_Port GPIOA
+#define DPH_Pin GPIO_PIN_0
+#define DPH_GPIO_Port GPIOB
+#define DPL_Pin GPIO_PIN_1
+#define DPL_GPIO_Port GPIOB
+#define DNH_Pin GPIO_PIN_11
+#define DNH_GPIO_Port GPIOA
+#define DNL_Pin GPIO_PIN_12
+#define DNL_GPIO_Port GPIOA
+#define LED_Pin GPIO_PIN_15
+#define LED_GPIO_Port GPIOA
+#define STDBY_Pin GPIO_PIN_3
+#define STDBY_GPIO_Port GPIOB
+#define DIR_Pin GPIO_PIN_4
+#define DIR_GPIO_Port GPIOB
+#define STEP_Pin GPIO_PIN_5
+#define STEP_GPIO_Port GPIOB
+#define INDEX_Pin GPIO_PIN_6
+#define INDEX_GPIO_Port GPIOB
+#define DIAG_Pin GPIO_PIN_7
+#define DIAG_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
